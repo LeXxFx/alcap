fed8b627cca10b8191fdcb8e76b11304156f91b1
Andriy Kondartyuk <gvakon@gmail.com>
2018-04-11 17:21:03 +0300

    add shop grid

--- html/assets/js/default.js
+++ html/assets/js/default.js
@@ -1,122 +1,82 @@
 ﻿var Main = function () {
+	var CatalogNavigation = function () {
+		var timer,
+			timeIn = [],
+			timeOut = [];
+		$('.col-catalog').on('mouseenter', function (e) {
+			clearTimeout(timer);
+			$(this).addClass('col-catalog--open');
+		}).on("mouseleave", function () {
+			timer = setTimeout(function () {
+				$('.col-catalog').removeClass('col-catalog--open');
+			}, 200);
+		});
 
-	var mainSlider = function () {
-        var slNav = $('.slider-nav__items');
-        if (slNav.length) {
-            slNav.slick({
-                slidesToShow: 6,
-                slidesToScroll: 6,
-                autoplay: false,
-                arrows: false,
-                asNavFor: '.slider',
-                focusOnSelect: true,
-                responsive: [
-                    {
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                            arrows: true
-                        }
-                    },
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 1,
-                            arrows: true
-                        }
-                    },
-                    {
-                        breakpoint: 767,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1,
-                            arrows: true
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                            arrows: true
-                        }
-                    }
-                ]
-            });
+		$(".col-catalog .catalog-menu .has-child").hover(function () {
+			var that = $(this),
+				uid = that.index();
 
-            var preview = $('.slider');
-            preview.slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                adaptiveHeight: true,
-                asNavFor: '.slider-nav__items'
-            });
+			timeIn[uid] = setTimeout(function () {
+				that.addClass('has-child--open');
+			}, 200);
+			if (timeOut[uid] !== undefined) {
+				clearTimeout(timeOut[uid]);
+			};
+		}, function () {
+			var that = $(this),
+				uid = that.index();
 
-        }
-    };
+			if (timeIn[uid] !== undefined) {
+				clearTimeout(timeIn[uid]);
+			};
+			timeOut[uid] = setTimeout(function () {
+				that.removeClass('has-child--open');
+			}, 200);
+		});
 
-	var maskedInput = function() {
-		$('.masked-phone').mask('9 (999) 999-99-99');
-    };
-    
-	var changeForm = function() {
-		$('body').find('.form-type .type-item').on('click', function(e){
-            e.preventDefault();
-            var that = $(this),
-                currentForm = $('body').find('#' + that.data('target'));
+		$('.catalog-menu').on('click', '.submenu__close', function (e) {
+			e.preventDefault();
+			var that = $(this);
+			that.closest('.col-catalog').removeClass('col-catalog--open');
+		});
 
-            that.siblings().removeClass('active');
-            that.addClass('active');
-            
-            currentForm.siblings().removeClass('active');
-            currentForm.addClass('active');
-        })
-    };
-    
-    var standsSlider = function () {
-        var stands = $('.stands-list');
-        if (stands.length) {
-            stands.slick({
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                autoplay: false,
-                arrows: true,
-                dots: true,
-                responsive: [
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2,
-                            dots: false
-                        }
-                    },
-                    {
-                        breakpoint: 768,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                            dots: false
-                        }
-                    }
-                ]
-            });
-        }
-    };
+		$('#header').on('click', '.col-catalog', function (e) {
+			$(this).toggleClass('col-catalog--open');
+		});
+	}
+
+	var Tooltips = function () {
+		$('[title]').attr("data-rel", "tooltip");
+		$("[data-rel='tooltip']")
+			.attr("data-placement", "top")
+			.attr("data-content", function () {
+				return $(this).attr("title")
+			})
+			.removeAttr('title');
+
+
+		var showPopover = function () {
+			$(this).popover('show');
+		};
+		var hidePopover = function () {
+			$(this).popover('hide');
+		};
+		$("[data-rel='tooltip']").popover({
+			trigger: 'manual'
+		}).click(showPopover).hover(showPopover, hidePopover);
+	}
+
+	var ScrollbarRail = function() {
+		$(document).ready(function(){
+			$('.scrollbar-rail').scrollbar();
+		});
+	}
 
 	return {
-        init: function () {
-            mainSlider();
-            maskedInput();
-            changeForm();
-            standsSlider();
-        }
-    };
+		init: function () {
+			CatalogNavigation();
+			Tooltips();
+			ScrollbarRail();
+		}
+	};
 }();
-
-$(document).ready(function ($) {
-    Main.init();
-});
